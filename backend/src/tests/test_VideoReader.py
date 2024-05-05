a2cdc7cb68e0b1ccbdb51bd1f718b28f1b619693
Katherine Zhang <156546096+zhkat@users.noreply.github.com>
2024-05-05 16:14:12 -0400

    Reorganize backend, add feature

---
+++ backend/src/tests/test_VideoReader.py
@@ -0,0 +1,24 @@
+import unittest
+from util.PoseEstimation import *
+from datamanagement.VideoReader import *
+
+class TestVideoReader(unittest.TestCase):
+
+    def test_detect_person(self):
+        # test 1:
+
+        # Load a pre-trained model for Pose Estimation
+        model_path = "yolov8n-pose.pt"
+        pose_model = PoseEstimation(model_path)
+
+        # Open the image file
+        img_path = "../input_videos/run.png"
+        data_manager = DataManagement(img_path)
+
+        self.assertTrue(
+            data_manager.detect_person(pose_model)
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
