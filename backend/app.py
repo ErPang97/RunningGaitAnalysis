b36bc22c0a59bd3e42f1827049fd621727554208
Samuel Lee <leesamuel423@gmail.com>
2024-05-06 10:56:03 -0400

    connect frontend to backend

---
+++ backend/app.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, jsonify
+from flask_cors import CORS
+from flask import send_from_directory
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = '../temp/uploads/'
+
+# only allow specific video file uploads
+ALLOWED_EXTENSIONS = {'mp4'}
+
+app = Flask(__name__)
+CORS(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploadVideo', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        # saves the uploaded file in the temp folder
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify({'message': 'File uploaded successfully'}), 200
+    else:
+        return jsonify({'message': 'File upload failed'}), 400
+
